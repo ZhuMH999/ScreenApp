21942e2b70097608beaa37cc6bc39741c128e6a8
Ryan <zhumh.ryan@gmail.com>
2026-07-04 10:04:55 +0800

    Initial Commit

---
+++ ScreenApp/ScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAppApp.swift
+//  ScreenApp
+//
+//  Created by Ryan on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
